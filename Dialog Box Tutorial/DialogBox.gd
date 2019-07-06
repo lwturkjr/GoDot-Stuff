a058f5a1fb9a01bace6a94a658b21dc5fd108087
lwturkjr <lwturkjr@SgtSegFault.home>
2019-07-06 01:25:55 -0400

    Dialog Box Tutorial, going through tutorials on YouTube, by Heartbeast

---
+++ Dialog Box Tutorial/DialogBox.gd
@@ -0,0 +1,23 @@
+# Dialog Box
+extends RichTextLabel
+
+var dialog = ["Hey! My name is Lloyd.", "Welcome to Godot dialog tutorial."]
+var page = 0
+
+func _ready():
+	set_bbcode(dialog[page])
+	set_visible_characters(0)
+	set_process_input(true)
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if get_visible_characters() > get_total_character_count():
+			if page < dialog.size() - 1:
+				page += 1
+				set_bbcode(dialog[page])
+				set_visible_characters(0)
+		else:
+			set_visible_characters(get_total_character_count())
+
+func _on_Timer_timeout():
+	set_visible_characters(get_visible_characters()+1)
